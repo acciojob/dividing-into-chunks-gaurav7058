fd2449fd1285e9e7e71da40e977b9a028b8d8e6a
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-07-12 12:23:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	for (let i = 0; i < n; i++) {
+		let str=[];
+		for (let j = i; j < n; j++) {
+			str.push(arr[j])
+		}	
+    console.log(str)
+	}
 };
 
-const n = prompt("Enter n: ");
+// const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));

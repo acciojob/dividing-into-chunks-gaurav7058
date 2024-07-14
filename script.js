86a3a08d6c05efc6d9b725db6963348a81446670
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-07-14 12:07:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,14 +4,22 @@ const divide = (arr, n) => {
   // Write your code here
 	let str=[]
   for (let i = 0; i < n; i++) {
-      for (let j = i+1; j < n; j++) {
-          if (arr[i]+arr[j]<=5) {
-              str.push(arr[i],arr[j])
-          }
+      for (let j = i; j < n; j++) {
+          let subArray=arr.slice(i,j+1)
+		  if (subArray.length>0) {
+			  let sum=0;
+			  for (let t of subArray) {
+			  	sum+=t;
+			  }
+			  if (sum<5) {
+			      console.log(subArray)
+			  	
+			  }
+		  	
+		  }
       }
-      console.log(str)
   }
 };
 
-// const n = prompt("Enter n: ");
+const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));
